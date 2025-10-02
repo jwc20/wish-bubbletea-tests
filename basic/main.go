fb0e3ba6c11e0da6d6f8ceb08bb1a5743aa2132c
jw <choi.jae100@gmail.com>
2025-10-02 03:00:23 -0700

    add text input

--- basic/main.go
+++ basic/main.go
@@ -1,23 +1,25 @@
-package main 
-
+package main
 
 import (
 	"fmt"
 	"log"
 	// "os"
 
-	// "github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type model struct {
-	payload string
+	// payload string
+	ti textinput.Model // text input model will have its own view, method, and etc methods
 }
 
 func initialModel() model {
+	ti := textinput.New()
 	return model{
-		"DEFAULT VALUE",
+		ti,
 	}
+
 }
 
 // automatically run when p.Run()
@@ -27,11 +29,11 @@ func (m model) Init() tea.Cmd {
 
 // automatically run when p.Run()
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	// this method returns tea.Model beacuause this is not a pointer/receiver 
+	// this method returns tea.Model beacuause this is not a pointer/receiver
 	// any changes made to m model will not persist outside of this method scope because it's passed by copy
 	// this meathod is like an event handler (pub/sub ood pattern) where it listens for events (in the form of t.message)
 	// return m, nil
-	
+
 	if val, ok := msg.(tea.KeyMsg); ok {
 		key := val.String()
 		// os.WriteFile("output.log", []byte(key), 0644)
@@ -39,19 +41,23 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		if key == "ctrl+c" {
 			return m, tea.Quit
 		}
-
-		m.payload = key
 	}
 
-	return m, nil
+	var cmd tea.Cmd
+	m.ti, cmd = m.ti.Update(msg)
+
+	return m, cmd
 }
 
 func (m model) View() string {
-	return m.payload
+	// return m.payload
+	// return m.ti.View()
+	output := fmt.Sprintf("Name?\n\n%v", m.ti.View())
+	return output
 }
 
 func main() {
-	fmt.Println("foo bar")
+	//fmt.Println("foo bar")
 	p := tea.NewProgram(initialModel()) // create new program, tea.NewProgram() returns a pointer to a pointer to a t.program structure
 
 	// p.Run() returns: t.model and error
